36db3e602c78708fa72a675666b95ae956e19991
Eric Mora <eric.mora@travelwifi.com>
2025-05-14 12:19:20 -0600

    feat: redux store init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { CategoryList } from './components/organisms/testParaStore';
+
+function App() {
+  return (
+    <div className="App">
+      <CategoryList />
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+module.exports = {
+  // …
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+}
